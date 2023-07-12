5ca3e36255f46fa85c870aef124469a2e02fcf28
Jihun Choi <jihunchoi998@gmail.com>
2023-07-11 20:49:43 -0400

    ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,6 +103,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  Config.force_ssl = true
+  config.force_ssl = true
 
 end

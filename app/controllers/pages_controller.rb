390959344f1c7fa251b5cfe4cab345e61b7d974e
Jihun Choi <jihunchoi998@gmail.com>
2023-07-20 19:32:53 -0400

    finally fix slider

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,10 +6,12 @@ class PagesController < ApplicationController
     #@posts_hot = Post.order(created_at: :asc).limit(4)
     @posts_hot = Post.order(views: :desc).limit(16)
     @posts_latest = Post.order(created_at: :desc).limit(16)
-    @posts_choice = Post.where(id: [13, 30, 45, 46, 
-                                    35,11,29,47,
-                                    38,25,7,48,
-                                    43,23,49,50])
+    @posts_innovative = Post.where(id: [13, 30, 45, 46])
+    @posts_sustain = Post.where(id: [35,11,29,47]) 
+    @posts_art = Post.where(id: [38,25,7,48]) 
+    @posts_culture = Post.where(id: [43,23,49,50])
+
+    
 
 
   end

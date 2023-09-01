0c4245a199b1f4ecd48c9f1a8054c03d447f882b
Jihun Choi <jihunchoi998@gmail.com>
2023-08-31 20:50:54 -0400

    going back to 587

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.porkbun.com',
-    port: 465,
+    port: 587,
     domain: 'mokkalog.com',
     user_name: 'admin@mokkalog.com',
     password: '@Coffeecake64',

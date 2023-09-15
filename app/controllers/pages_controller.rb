66885e6346ed43ae9efebbf5540f035ce2a70562
Jihun Choi <jihunchoi998@gmail.com>
2023-09-15 12:26:56 -0400

    changed innovatiev

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
     #@posts_hot = Post.order(created_at: :asc).limit(4)
     @posts_hot = Post.order(views: :desc).limit(16)
     @posts_latest = Post.order(created_at: :desc).limit(16)
-    @posts_innovative = Post.where(id: [13, 30, 385, 382])
+    @posts_innovative = Post.where(id: [13, 30, 387, 382])
     @posts_sustain = Post.where(id: [35,19,29,375]) 
     @posts_art = Post.where(id: [385,25,7,194]) 
     @posts_culture = Post.where(id: [43,23,49,380])

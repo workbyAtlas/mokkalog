0a9f1ee5235255d49eec32db056de75e9c7eaaf2
Jihun Choi <jihunchoi998@gmail.com>
2023-12-07 16:50:53 -0500

    not sure if this is going to change anything

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,7 +3,7 @@
 class Users::SessionsController < Devise::SessionsController
 
   #before_action :configure_sign_in_params, only: [:create]
-  # before_action :lockdown, only: %i[create]
+  
   # GET /resource/sign_in
   # def new
   #   super

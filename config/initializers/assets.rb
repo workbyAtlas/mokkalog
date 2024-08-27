bf05fd61e14570af40c9c908ae82862f3186a508
Jihun Choi <workbyatlas@gmail.com>
2024-08-26 21:26:16 -0400

    final for real maybe

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.paths << Emoji.images_path
 
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
 Rails.application.config.assets.precompile += %w( .svg .eot .woff .woff2 .ttf )
 
 
